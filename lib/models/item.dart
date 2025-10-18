932273df258e0a98f7921021d9298cdfc90473d9
MarcosOlcinaUPV <molccom@upv.edu.es>
2025-10-18 11:55:38 +0200

    Añadir clase item para modelos 3D

---
+++ lib/models/item.dart
@@ -0,0 +1,15 @@
+class Item {
+  final String id;
+  final String titulo;
+  final String descripcion;
+  final String imagenPath;
+  final String etiqueta;
+
+  Item({
+    required this.id,
+    required this.titulo,
+    required this.descripcion,
+    required this.imagenPath,
+    required this.etiqueta,
+  });
+}
